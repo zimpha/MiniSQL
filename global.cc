14ab0776719726024727cf70744d13f05bc0e61c
zimpha <zimpha@gmail.com>
2014-11-09 17:22:52 +0800

    add global header

---
+++ global.cc
@@ -0,0 +1,113 @@
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include "global.h"
+
+Table::Table() {
+}
+
+Table::Table (const std::string &name, const std::vector<AttrType> &attributes) {
+    this->entrySize = 0;
+    this->name = name;
+    for (size_t i = 0; i < attributes.size(); ++ i) {
+        switch (attributes[i].type) {
+            case 0: entrySize += sizeof(int); break;
+            case 1: entrySize += sizeof(float); break;
+            case 2: entrySize += attributes[i].length; break;
+            default: assert(false);
+        }
+    }
+    this->attributes = attributes;
+}
+
+void Table::write() {
+    std::ofstream fout((name).c_str());
+    fout << name << std::endl;
+    fout << entrySize << std::endl;
+    fout << attributes.size() << std::endl;
+    for (size_t i = 0; i < attributes.size(); ++ i) {
+        fout << attributes[i].name << std::endl;
+        fout << attributes[i].type << std::endl;
+        fout << attributes[i].length << std::endl;
+        fout << attributes[i].unique << std::endl;
+        fout << attributes[i].indices.size() << std::endl;
+        std::set<std::string> &indices = attributes[i].indices;
+        for (auto &x : indices) {
+            fout << x << std::endl;
+        }
+        fout << std::endl;
+    }
+    fout.close();
+}
+
+bool AttrType::operator == (const AttrType &rhs) const {
+    return type == rhs.type && length == rhs.length && unique == rhs.unique &&
+           indices == rhs.indices && name == rhs.name;
+}
+
+element::element(): type(-1) { }
+element::element(int x): i(x), type(0) {}
+element::element(float x): d(x), type(1) {}
+element::element(double x): d(x), type(1) {}
+element::element(std::string x): s(x), type(2) {}
+element::element(const char *x): s(x), type(2) {}
+
+void element::print() {
+    switch (type) {
+        case 0: std::cout << i; return;
+        case 1: std::cout << d; return;
+        case 2: std::cout << s; return;
+        default: assert(false);
+    }
+}
+
+bool element::operator < (const element &rhs) const {
+    assert(type == rhs.type);
+    switch (type) {
+        case 0: return i < rhs.i;
+        case 1: return d < rhs.d;
+        case 2: return s < rhs.s;
+        default: assert(false);
+    }
+}
+
+bool element::operator == (const element &rhs) const {
+    assert(type == rhs.type);
+    switch (type) {
+        case 0: return i == rhs.i;
+        case 1: return fabs(d - rhs.d) < 1e-8;
+        case 2: return s == rhs.s;
+        default: assert(false);
+    }
+}
+
+bool element::operator > (const element &rhs) const {
+    return !(*this < rhs || *this == rhs);
+}
+
+bool element::operator >= (const element &rhs) const {
+    return *this > rhs || *this == rhs;
+}
+
+bool element::operator <= (const element &rhs) const {
+    return *this < rhs || *this == rhs;
+}
+
+bool element::operator != (const element &rhs) const {
+    return !(*this == rhs);
+}
+
+Response::Response(): succeed(true) {}
+Response::Response(std::string data): succeed(false), info(data) {}
+Response::Response(std::vector<std::vector<element> > res): succeed(false), result(res) {}
+
+//#define LOCAL_TEST
+#ifdef LOCAL_TEST
+
+int main() {
+    return 0;
+}
+
+#endif
---
+++ global.h
@@ -0,0 +1,54 @@
+#ifndef _GLOBAL_H
+#define _GLOBAL_H
+
+#include <set>
+#include <string>
+#include <vector>
+
+struct AttrType {
+    std::string name;
+    int type, length;
+    bool unique;
+    std::set<std::string> indices;
+    bool operator == (const AttrType &rhs) const;
+};
+
+struct Table {
+    int entrySize;
+    std::string name;
+    std::vector<AttrType> attributes;
+    Table (const std::string &name, const std::vector<AttrType> &attributes);
+    Table ();
+    void write();
+};
+
+struct element {
+    int i;
+    double d;
+    std::string s;
+    int type; // 0: int, 1: double, 2: string, -1:invalid
+    element();
+    element(int x);
+    element(double x);
+    element(float x);
+    element(std::string x);
+    element(const char *d);
+    bool operator < (const element &rhs) const;
+    bool operator == (const element &rhs) const;
+    bool operator > (const element &rhs) const;
+    bool operator >= (const element &rhs) const;
+    bool operator <= (const element &rhs) const;
+    bool operator != (const element &rhs) const;
+    void print();
+};
+
+struct Response {
+    bool succeed;
+    std::string info;
+    std::vector<std::vector<element> > result;
+    Response();
+    Response(std::string data);
+    Response(std::vector<std::vector<element> > res);
+};
+
+#endif
